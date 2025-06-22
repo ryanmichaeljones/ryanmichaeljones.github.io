3cec58e1d8617abf9cd7aa4a552cdc4710def5b5
Ryan Jones <ryanmichaeljonesbusiness@gmail.com>
2025-06-22 12:01:56 +0100

    Removed plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { cloudflare } from "@cloudflare/vite-plugin";
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), cloudflare()],
+    plugins: [react()],
     // base: '/ryanmichaeljones.github.io/',
     resolve: {
         alias: {
