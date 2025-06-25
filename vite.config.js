7adacc53ab99b6fdaa0f75ab6cba4240bdc233f7
Ryan Jones <ryanmichaeljonesbusiness@gmail.com>
2025-06-25 19:06:30 +0100

    Remove unused middleware for rewriting slashes to index.html

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-const rewriteSlashToIndexHtml = () => {
-    return {
-        name: 'rewrite-slash-to-index-html',
-        apply: 'serve',
-        enforce: 'post',
-        configureServer(server) {
-            // rewrite / as index.html
-            server.middlewares.use('/', (req, _, next) => {
-                if (req.url === '/') {
-                    req.url = '/index.html'
-                }
-                next()
-            })
-        },
-    }
-}
-
 // https://vite.dev/config/
 export default defineConfig({
     appType: 'spa',
